1372e688726dd9e3acb0b3c4ce9be3dab8be6375
sinhyuk <rlatls1997@gmail.com>
2021-05-26 03:01:28 +0900

    programmers_두 정수 사이의 합

---
+++ src/programmers_prac/lessons12912.java
@@ -0,0 +1,11 @@
+package programmers_prac;
+//연습문제 > 두 정수 사이의 합
+public class lessons12912 {
+    public long solution(int a, int b) {
+        return (long) (a + b) * (Math.abs(a - b) + 1) / 2;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
