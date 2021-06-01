8522e5e966f0585bd12d9acbea91aa07f699a661
sinhyuk <rlatls1997@gmail.com>
2021-06-01 22:24:26 +0900

    programmers_문자열을 정수로 바꾸기

---
+++ src/programmers_prac/lessons12925.java
@@ -0,0 +1,9 @@
+package programmers_prac;
+//연습문제 > 문자열을 정수로 바꾸기
+public class lessons12925 {
+    public int solution(String s) {
+        int answer = 0;
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+}
