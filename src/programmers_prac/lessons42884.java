fcae77b1a04e530ab36205fceeaa1bd264e5a455
sinhyuk <rlatls1997@gmail.com>
2021-12-27 23:12:00 +0900

    programmers_탐욕법(Greedy)_단속카메라

--- src/programmers_prac/lessons42884.java
+++ src/programmers_prac/lessons42884.java
@@ -2,18 +2,17 @@ package programmers_prac;
 //탐욕법(Greedy)
 //단속카메라
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class lessons42884 {
     public int solution(int[][] routes) {
-        int answer = 1;
+        int answer = 0;
         int cam = Integer.MIN_VALUE;
 
         Arrays.sort(routes, (o1, o2) -> {
             return o1[1] - o2[1];
         });
 
-        for (int i = 1; i < routes.length; i++) {
+        for (int i = 0; i < routes.length; i++) {
             int start = routes[i][0];
             int end = routes[i][1];
 
