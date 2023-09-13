21fdb5019fe106837c344213dbf44368386f4629
rlatls1997 <rlatls1997@gmail.com>
2023-09-13 20:03:24 +0900

    chore : gitignore renewal and project setting

---
+++ src/programmers_prac/Main.java
@@ -0,0 +1,12 @@
+package programmers_prac;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] park = {"OSO", "OOO", "OXO", "OOO"};
+        String[] routes = {"E 2", "S 3", "W 1"};
+
+
+        lessons172928 lessons172928 = new lessons172928();
+        lessons172928.solution(park, routes);
+    }
+}
